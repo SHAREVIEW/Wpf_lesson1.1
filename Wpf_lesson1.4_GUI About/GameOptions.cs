0dcd70aa9d4870a38ee4115d966114531d186265
david <2657070472@qq.com>
2018-11-14 11:05:04 +0800

    2018.11.14_p394_14.4.3数据绑定

---
+++ Wpf_lesson1.4_GUI About/GameOptions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Wpf_lesson1._4_GUI_About
+{
+    //保存Option窗口各值的属性
+    [Serializable]
+    public class GameOptions
+    {
+        public bool PlayerAgainstComputer { get; set; }
+        public int NumberOfPlayers { get; set; }
+        public int MinutesBeforeLoss { get; set; }
+        public ComputerSkillLevel ComputerSkill { get; set; }
+    }
+    [Serializable]
+    public enum ComputerSkillLevel   //枚举Level
+    {
+        Dumb, Good, Cheats
+    }
+}
---
+++ Wpf_lesson1.4_GUI About/NumberOfPlayers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.ObjectModel;
+
+namespace Wpf_lesson1._4_GUI_About
+{
+    public class NumberOfPlayers : ObservableCollection<int>
+    {
+        /*
+         *      public NumberOfPlayers
+            : base()
+    {
+        add(2);
+        add(3);
+        add(4);
+
+    }
+         
+             */
+
+
+    }
+}
